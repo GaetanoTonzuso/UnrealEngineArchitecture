217dc1269b838643d2636b138ce1bd4daf0cc433
djchucky <gaetano.tonzuso@gmail.com>
2025-12-06 23:21:12 +0100

    Added Goal Class

---
+++ Source/UnrealEngineArch/Private/Goal.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Goal.h"
+
+#include "Ship.h"
+
+// Sets default values
+AGoal::AGoal()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGoal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGoal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AGoal::NotifyHit(class UPrimitiveComponent* MyComp, 
+	AActor* Other, 
+	class UPrimitiveComponent* OtherComp, 
+	bool bSelfMoved, 
+	FVector HitLocation, 
+	FVector HitNormal, 
+	FVector NormalImpulse, 
+	const FHitResult& Hit)
+{
+	Super::NotifyHit(MyComp, Other, OtherComp, bSelfMoved, HitLocation, HitNormal, NormalImpulse, Hit);
+
+	if (Other && Other != this && Other->IsA(AShip::StaticClass()))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Goaaaaal!"));
+	}
+
+}
+
---
+++ Source/UnrealEngineArch/Public/Goal.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+//TODO: Notify the goal that has been hit (Physics Hit)
+//TODO: By the ship actor
+//TODO: Stop timer?? We need a timer object
+//TODO: Activate Celebration
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Goal.generated.h"
+
+UCLASS()
+class UNREALENGINEARCH_API AGoal : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGoal();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void NotifyHit(class UPrimitiveComponent* MyComp, 
+		AActor* Other, 
+		class UPrimitiveComponent* OtherComp, 
+		bool bSelfMoved, FVector HitLocation, 
+		FVector HitNormal, 
+		FVector NormalImpulse, 
+		const FHitResult& Hit) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+private:	
+	
+
+};
